eac97fc706256def30383b7d02ea33d97fbd195a
DESKTOP-BA6IJ4R\Tim <timpungracic@gmail.com>
2017-09-03 17:23:04 +0200

    no message

---
+++ Assets/Scripts/triggerskripta.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerskripta : MonoBehaviour {
+    public GameObject kista;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        kista.transform.position = new Vector3(kista.transform.position.x, kista.transform.position.y + 10, kista.transform.position.z);
+    }
+
+}
