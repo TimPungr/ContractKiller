651010f1686ab34487a939363838c65112d7151b
DESKTOP-BA6IJ4R\Tim <timpungracic@gmail.com>
2017-09-10 10:52:40 +0200

    no message

---
+++ Assets/Scripts/DisableRagDoll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRagDoll : MonoBehaviour {
+
+    Collider[] rigColliders;
+    Rigidbody[] rigRigidbodies;
+
+    // Use this for initialization
+    void Start () {
+
+
+        {
+            rigColliders = GetComponentsInChildren<Collider>();
+            rigRigidbodies = GetComponentsInChildren<Rigidbody>();
+        }
+
+
+            //wait 2-3 seconds.
+            /*foreach (Collider col in rigColliders)
+            {
+                col.enabled = false;
+            }*/
+
+            foreach (Rigidbody rb in rigRigidbodies)
+            {
+                rb.isKinematic = true;
+            }
+        }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
