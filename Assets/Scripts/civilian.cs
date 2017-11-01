4ae4e0d650f9983527dcb07d558d7ed708c551c1
DESKTOP-BA6IJ4R\Tim <timpungracic@gmail.com>
2017-11-01 17:35:40 +0100

    no message

--- Assets/Scripts/civilian.cs
+++ Assets/Scripts/civilian.cs
@@ -9,9 +9,9 @@ public class civilian : MonoBehaviour {
     public float distance;
     public float wanderTime = 0f;
     private float wanderTimer;
-    private bool scared;
+    private bool scared = false;
     public LayerMask mask;
-
+    
 	// Use this for initialization
 	void Start () {
         agent = GetComponent<NavMeshAgent>();
@@ -20,11 +20,12 @@ public class civilian : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        wanderTime -= Time.deltaTime;
-        if(wanderTime == 0 && !scared)
+        wanderTimer -= Time.deltaTime;
+        if(wanderTimer <= 0 && !scared)
         {
-            wanderTime = wanderTimer;
-            agent.SetDestination(Wander(transform.position,distance,mask));
+            wanderTimer = wanderTime;
+            agent.SetDestination(Wander(transform.position, distance, mask));
+            //Debug.Log(agent.destination);
         }
 	}
 
@@ -37,6 +38,8 @@ public class civilian : MonoBehaviour {
         NavMeshHit navHit;
 
         NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+
+        //Debug.Log(navHit.position);
 
         return navHit.position;
     }
